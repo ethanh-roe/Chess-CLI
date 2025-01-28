fbbe1a6f80292a099e07fd3b53c9d8a0c5cf6ef4
ethanhroe <ethan.h.roe@gmail.com>
2025-01-28 12:42:39 -0600

    Added struct for board and enum for status of king

---
+++ util.h
@@ -0,0 +1,43 @@
+/**
+ * Chess-CLI
+ * 
+ * 1/28/2025
+ * 
+ * util.h
+ * 
+ * Ethan Roe
+ */
+
+#pragma once
+
+#include <stdio.h>
+#include <string.h>
+
+#ifndef UTIL_H_
+#define UTIL_H_
+
+#define WHITE 2
+#define BLACK 1
+#define EMPTY 0
+
+// struct for default board template
+typedef struct board_template {
+    char current;
+    char currentSquare[2];
+    bool occupied;
+    int color;
+} board_template;
+
+typedef enum KingStatus {
+    check,
+    checkmate,
+    clear
+} KingStatus;
+
+//
+// function prototypes for main lib
+//
+
+void initBoard(board_template board[][8], unsigned k, char col);
+
+#endif
