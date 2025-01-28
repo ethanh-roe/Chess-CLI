b8a928b1f548978a2cb3862945dbd0c471c0d63e
ethanhroe <ethan.h.roe@gmail.com>
2025-01-28 13:04:14 -0600

    Included more libs

--- util.h
+++ util.h
@@ -1,17 +1,22 @@
 /**
  * Chess-CLI
- * 
+ *
  * 1/28/2025
- * 
+ *
  * util.h
- * 
+ *
  * Ethan Roe
  */
 
 #pragma once
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <unistd.h>
 
 #ifndef UTIL_H_
 #define UTIL_H_
@@ -20,15 +25,22 @@
 #define BLACK 1
 #define EMPTY 0
 
+#define MOS 17
+# define KRED  "\x1B[31m"
+# define KYEL  "\x1B[33m"
+# define RESET "\033[0m"
+
 // struct for default board template
-typedef struct board_template {
+typedef struct board_template
+{
     char current;
     char square[2];
     bool occupied;
     int color;
 } board_template;
 
-typedef enum KingStatus {
+typedef enum KingStatus
+{
     check,
     checkmate,
     clear
@@ -39,5 +51,7 @@ typedef enum KingStatus {
 //
 
 void initBoard(board_template board[][8], unsigned r, char c);
+
+void printBoard(board_template board[][8], const char p);
 
 #endif
